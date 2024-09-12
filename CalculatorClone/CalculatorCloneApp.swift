f70cfec81efe69d027fc89b22655010c47bc4b3e
Rahul Narayanan <rahulon@icloud.com>
2024-09-12 00:30:54 -0400

    Initial Commit

---
+++ CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by Rahul on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculatorClone
+//
+//  Created by Rahul on 9/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
